29aec803ea2724fd8e4661bbe1c65e248de1d9dc
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-04-26 20:05:55 -0300

    Use formatter in dates

--- src/interfaces/IPostInterface.ts
+++ src/interfaces/IPostInterface.ts
@@ -1,12 +1,12 @@
 export interface IPost {
-    id: string;
-    title: string;
-    body: string;
-    created_at: string;
-    number: number;
-    html_url: string;
-    comments: number;
-    user: {
-      login: string;
-    };
-  }
+  id: string;
+  title: string;
+  body: string;
+  created_at: Date;
+  number: number;
+  html_url: string;
+  comments: number;
+  user: {
+    login: string;
+  };
+}
--- src/pages/Blog/PostCard/index.tsx
+++ src/pages/Blog/PostCard/index.tsx
@@ -1,40 +1,49 @@
 import { IPost } from "../../../interfaces/IPostInterface";
+import { formatDateToPassedTimeInDays, formatDateToPassedTimeInHours, formatPassedDaysToString, formatPassedHoursToString } from "../../../utils/formatter";
 import { Header, PostCardContainer, PostCardSpan } from "./styles";
 
-interface IPostCardProps{
+interface IPostCardProps {
     post: IPost;
 }
 
-export function PostCard({post}: IPostCardProps) {
+export function PostCard({ post }: IPostCardProps) {
     const postBodySpans = post.body.split("\r\r");
 
     const postRoute = `http://localhost:5173/post/${post.number}`;
 
+    let passedTime = formatDateToPassedTimeInDays(post.created_at);
+    let formattedPassedTime = formatPassedDaysToString(passedTime);
+
+    if (passedTime === 0) {
+        passedTime = formatDateToPassedTimeInHours(post.created_at);
+        formattedPassedTime = formatPassedHoursToString(passedTime);
+    }
+
     return (
         <PostCardContainer>
-            
-                <Header>
-                    <a href={postRoute}>
-                        <strong>
-                            {post.title}
-                        </strong>
-                    </a>
-                    <span>
-                            {post.created_at}
-                     </span>
-                </Header>
 
-                <PostCardSpan>
-                    {
-                        postBodySpans.map((bodySpan) => {
-                            return(
-                                <span>
-                                    {bodySpan}
-                                </span>
-                            )
-                        })
-                    }
-                </PostCardSpan>
+            <Header>
+                <a href={postRoute}>
+                    <strong>
+                        {post.title}
+                    </strong>
+                </a>
+                <span>
+                    {formattedPassedTime}
+                </span>
+            </Header>
+
+            <PostCardSpan>
+                {
+                    postBodySpans.map((bodySpan) => {
+                        return (
+                            <span>
+                                {bodySpan}
+                            </span>
+                        )
+                    })
+                }
+            </PostCardSpan>
         </PostCardContainer>
     )
 }
