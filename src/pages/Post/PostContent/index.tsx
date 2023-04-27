d08503c9367cf0201508b43907233df1f0711764
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-04-26 21:24:20 -0300

    Implementation of watch form and validation

--- src/pages/Post/PostContent/index.tsx
+++ src/pages/Post/PostContent/index.tsx
@@ -1,38 +1,34 @@
-import { PostCode, PostContentContainer, StyledPostContent } from "./styles";
+import ReactMarkDown from "react-markdown";
+import { PostContentContainer, StyledPostContent } from "./styles";
 
-interface PostContentProps{
+interface PostContentProps {
     body: string;
 }
 
-export function PostContent({body}: PostContentProps){
+export function PostContent({ body }: PostContentProps) {
     const bodyPhrases = body.split("\r\n");
     const filteredBodyPhrases = bodyPhrases.filter(phrase => phrase !== "");
 
-    return(
+    let i = 0;
+
+    return (
         <PostContentContainer>
             <StyledPostContent>
 
                 <div>
-                    {filteredBodyPhrases.map((phrase) => {
-                        return (
-                            <span>
-                                {phrase} <br/> <br/>
-                            </span>
-                        )
-                    })}
+                    {
+                        filteredBodyPhrases.map((phrase) => {
+                            return (
+                                <>
+                                    <ReactMarkDown>
+                                        {phrase}
+                                    </ReactMarkDown>
+                                    <br />
+                                </>
+                            )
+                        })
+                    }
                 </div>
-
-                <PostCode>
-                    <pre>
-                        let foo = 42;   // foo is now a number
-                    </pre>
-                    <pre>
-                        foo = 'bar';    // foo is now a string
-                    </pre>
-                    <pre>
-                        foo = true;     // foo is now a boolean
-                    </pre>
-                </PostCode>
 
             </StyledPostContent>
         </PostContentContainer>
