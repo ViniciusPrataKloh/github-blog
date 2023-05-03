e73c6afd45f4d74c3a3a9c36239e559a12412c65
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-05-03 18:01:46 -0300

    Edit markdown styles

--- src/pages/Post/PostContent/styles.ts
+++ src/pages/Post/PostContent/styles.ts
@@ -11,17 +11,32 @@ export const StyledPostContent = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
-
+    list-style: inherit;
+    
     color: ${(props) => props.theme["base-text"]};
 
     a{
         color: ${(props) => props.theme["brand-blue"]};
-        text-decoration-line: underline;
+        text-decoration-line: none;
+        transition: 0.2s;
+
+        &:hover{
+            text-decoration-line: underline;
+        }
+    }
+
+    p{
+        margin-top: 1.5rem;
     }
 
     strong, h1,h2,h3{
         color: ${(props) => props.theme["brand-blue"]};
         font-weight: bold;
+    }
+
+    ul {
+        list-style: inherit;
+        padding-left: 1.5rem;
     }
 
     pre{
